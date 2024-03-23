0743f5fbd025ea7a9b8d457417fac596da15b70a
Tiziano Santoro <tiziano88@gmail.com>
2024-03-23 23:12:00 +0000

    Multi system

--- flake.nix
+++ flake.nix
@@ -1,49 +1,54 @@
 {
-  description = "linc";
+  description = "lint";
   inputs = {
     # https://status.nixos.org/
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = github:oxalica/rust-overlay;
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      pkgs = import nixpkgs {
-        overlays = [ rust-overlay.overlays.default ];
-        system = "x86_64-linux";
-      };
-    in
-    {
-      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
-      defaultPackage.x86_64-linux =
-        pkgs.stdenv.mkDerivation {
-          name = "build";
-          src = self;
-          buildPhase = "echo fo";
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+          # system = "x86_64-linux";
         };
-      devShell.x86_64-linux =
-        pkgs.mkShell {
-          buildInputs = [
-            pkgs.cargo
-            pkgs.protobuf
-            pkgs.trunk
-            pkgs.just
-            pkgs.leptosfmt
-            pkgs.tailwindcss
-            (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-              extensions = [
-                "clippy"
-                "llvm-tools-preview"
-                "rust-analyzer"
-                "rust-src"
-                "rustfmt"
-              ];
-              targets = [ 
-                "wasm32-unknown-unknown" 
-                "x86_64-unknown-linux-musl" 
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        defaultPackage =
+          pkgs.stdenv.mkDerivation {
+            name = "build";
+            src = self;
+            buildPhase = "echo fo";
+          };
+        devShell =
+          pkgs.mkShell {
+            buildInputs = [
+              pkgs.cargo
+              pkgs.protobuf
+              pkgs.trunk
+              pkgs.just
+              pkgs.leptosfmt
+              pkgs.nixpkgs-fmt
+              pkgs.tailwindcss
+              (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+                extensions = [
+                  "clippy"
+                  "llvm-tools-preview"
+                  "rust-analyzer"
+                  "rust-src"
+                  "rustfmt"
                 ];
-            }))
-          ];
-        };
-    };
+                targets = [
+                  "wasm32-unknown-unknown"
+                  "x86_64-unknown-linux-musl"
+                ];
+              }))
+            ];
+          };
+      }
+    ));
 }
